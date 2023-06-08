827b9c1c16b8f32d08002381af42ae66cf5a84ac
kyukyutw <59384367+kyukyutw@users.noreply.github.com>
2023-06-08 18:39:41 +0800

    Update app.py

--- app.py
+++ app.py
@@ -62,6 +62,7 @@ def GetAppleMusicHtmlServiceTag(url):
 
 def GetAppleMusicJsonUrl(sJsonString):
     sJson = json.loads(sJsonString)
+    print(sJson)
     sRet = sJson[data][sections](0)[items](0)[contentDescriptor][url]
     print(sRet)
     return sRet
