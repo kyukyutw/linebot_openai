02180b72c9eefa6e6586b8f6eda62541e20219b8
kyukyutw <59384367+kyukyutw@users.noreply.github.com>
2023-06-12 20:31:58 +0800

    Update app.py

--- app.py
+++ app.py
@@ -60,6 +60,7 @@ def TranUrlWebpToPNG(webpUrl):
             'background': '#ffffff'
         }
         headers = {
+            'user-agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36',
             'Content-Encoding': 'gzip',
             'Content-Type': 'text/html;charset=UTF-8'
         }
