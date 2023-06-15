b17f78f7e56b6c9a551c16962d57b053a2dec57c
kyukyutw <59384367+kyukyutw@users.noreply.github.com>
2023-06-15 18:06:44 +0800

    Update app.py

--- app.py
+++ app.py
@@ -324,14 +324,18 @@ def handle_message(event):
     client_id = "05f738e527b6fea"
     SendImage = line_bot_api.get_message_content(event.message.id)
 
-    local_save = './Image/test0615.png'
+    print('message.id:' + event.message.id)
+    print(type(SendImage))
+    '''
+    local_save = '/Image/test0615.png'
     #local_save = './Image/' + event.message.id + '.png'
     with open(local_save, 'wb') as fd:
         for chenk in SendImage.iter_content():
             fd.write(chenk)
-
+    
     img_url = glucose_graph(client_id, local_save)
     print(img_url)
+    '''
     #line_bot_api.reply_message(event.reply_token, ImageSendMessage(original_content_url=img_url, preview_image_url=img_url))
     
 # 監聽所有來自 /callback 的 Post Request
